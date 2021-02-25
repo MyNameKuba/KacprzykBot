d92e8a86807cd3bcd61156777f3485a19ddacd3e
Phaseless <MyNameKuba@users.noreply.github.com>
2021-02-25 10:42:44 +0100

    Update html.js

--- html.js
+++ html.js
@@ -1,36 +1,11 @@
-const http = require('http');
-const fs = require('fs');
-const path = require('path');
+const app = require("express")();
 
-const hostname = 'kacprzykbot.herokuapp.com';
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
-const server = http.createServer((req, res) => {
-    console.log('Request for ' + req.url + ' by method ' + req.method);
-
-    if (req.method == 'GET') {
-        var fileUrl;
-        if (req.url == '/') fileUrl = '/index.html';
-        else fileUrl = req.url;
-
-        var filePath = path.resolve('./HTML' + fileUrl);
-        const fileExt = path.extname(filePath);
-        if (fileExt == '.html') {
-            fs.exists(filePath, (exists) => {
-                res.statusCode = 200;
-                res.setHeader('Content-Type', 'text/html');
-                fs.createReadStream(filePath).pipe(res);
-            });
-        }
-        else if (fileExt == '.css') {
-            res.statusCode = 200;
-            res.setHeader('Content-Type', 'text/css');
-            fs.createReadStream(filePath).pipe(res);
-        }
-    }
+app.get("", (req, res) => {
+  res.send("Hello world");
 });
 
-
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+app.listen(PORT, () => {
+  console.log(`App up at port ${PORT}`);
 });
