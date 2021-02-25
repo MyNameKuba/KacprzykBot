a681b73346daa81a3f5d44f237f2a366d73f05c7
Phaseless <MyNameKuba@users.noreply.github.com>
2021-02-25 11:37:31 +0100

    Final Fix

--- html.js
+++ html.js
@@ -1,13 +1,41 @@
-const app = require("express")();
+const http = require('http');
 const fs = require('fs');
+const path = require('path');
 
-const PORT = process.env.PORT || 3000;
+const hostname = 'localhost';
+const port = process.env.PORT || 3000;
 
-app.get("", (req, res) => {
-    res.setHeader('Content-Type', 'text/html');
-    fs.createReadStream('./HTML/index.html').pipe(res);
+const server = http.createServer((req, res) => {
+    console.log('Request for ' + req.url + ' by method ' + req.method);
+
+    if (req.method == 'GET') {
+        var fileUrl;
+        if (req.url == '/') fileUrl = '/index.html';
+        else fileUrl = req.url;
+
+        var filePath = path.resolve('./html' + fileUrl);
+        const fileExt = path.extname(filePath);
+        if (fileExt == '.html') {
+            fs.exists(filePath, (exists) => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'text/html');
+                fs.createReadStream(filePath).pipe(res);
+            });
+        }
+        else if (fileExt == '.css') {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'text/css');
+            fs.createReadStream(filePath).pipe(res);
+        }
+        else if (fileExt == '.js') {
+          res.statusCode = 200;
+          res.setHeader('Content-Type', 'text/js');
+          fs.createReadStream(filePath).pipe(res);
+      }
+    }
 });
 
-app.listen(PORT, () => {
-  console.log(`App up at port ${PORT}`);
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
 });
